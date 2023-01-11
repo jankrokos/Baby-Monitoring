6f2f31cee160a52a1fd891d8d04fd05fb7381305
Jan Krokos <jankrokos@gmail.com>
2023-01-11 23:22:14 +0100

    Initial commit

---
+++ app/src/main/java/com/example/babywatchpro/NotificationSettings.java
@@ -0,0 +1,6 @@
+package com.example.babywatchpro;
+
+public class NotificationSettings {
+    public static String HubName = "js-app-notifications";
+    public static String HubListenConnectionString = "Endpoint=sb://js-iot-project.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=/r2PH1PsDVQMRmKKuu3U1aI7SdO3EtYQ7CgTTl3Sm7s=";
+}
